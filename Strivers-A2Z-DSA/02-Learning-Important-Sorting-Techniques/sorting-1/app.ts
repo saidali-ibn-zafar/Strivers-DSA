e5214bbdb3afa5e087ca832b910fe1e0f3f221d0
saidali_ibn_zafar <120341849+saidali-ibn-zafar@users.noreply.github.com>
2024-02-10 20:56:25 +0100

    Add files via upload

---
+++ Strivers-A2Z-DSA/02-Learning-Important-Sorting-Techniques/sorting-1/app.ts
@@ -0,0 +1,44 @@
+// Selection Sort Algorithm
+// Problem Statement: Given an array of N integers,
+// write a program to implement the Selection sorting algorithm.
+const selectionSort = (arr: number[]): void => {
+  // n is length of an array
+  const n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    let temp = arr[minIndex];
+    arr[minIndex] = arr[i];
+    arr[i] = temp;
+  }
+  console.log(arr);
+};
+
+const arr = [13, 46, 24, 52, 20, 9];
+selectionSort(arr); // [ 9, 13, 20, 24, 46, 52 ]
+
+// = = = = =
+
+// Bubble Sort Algorithm
+// Problem Statement: Given an array of N integers,
+// write a program to implement the Bubble Sorting algorithm.
+const bubbleSort = (arr: number[]): void => {
+  const n = arr.length;
+
+  for (let i = n - 1; i >= 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  console.log(arr);
+};
+
+bubbleSort(arr); // [ 9, 13, 20, 24, 46, 52 ]
