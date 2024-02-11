97c1407b5c5b8da5849b03d818f6be8f24858eda
saidali_ibn_zafar <120341849+saidali-ibn-zafar@users.noreply.github.com>
2024-02-11 14:50:41 +0100

    added insertionSort

--- Strivers-A2Z-DSA/02-Learning-Important-Sorting-Techniques/sorting-1/app.ts
+++ Strivers-A2Z-DSA/02-Learning-Important-Sorting-Techniques/sorting-1/app.ts
@@ -1,6 +1,5 @@
 // Selection Sort Algorithm
-// Problem Statement: Given an array of N integers,
-// write a program to implement the Selection sorting algorithm.
+// Problem Statement: Given an array of N integers, write a program to implement the Selection sorting algorithm.
 const selectionSort = (arr: number[]): void => {
   // n is length of an array
   const n = arr.length;
@@ -24,13 +23,12 @@ selectionSort(arr); // [ 9, 13, 20, 24, 46, 52 ]
 // = = = = =
 
 // Bubble Sort Algorithm
-// Problem Statement: Given an array of N integers,
-// write a program to implement the Bubble Sorting algorithm.
+// Problem Statement: Given an array of N integers, write a program to implement the Bubble Sorting algorithm.
 const bubbleSort = (arr: number[]): void => {
   const n = arr.length;
 
   for (let i = n - 1; i >= 0; i--) {
-    for (let j = 0; j < i - 1; j++) {
+    for (let j = 0; j < i; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
@@ -42,3 +40,24 @@ const bubbleSort = (arr: number[]): void => {
 };
 
 bubbleSort(arr); // [ 9, 13, 20, 24, 46, 52 ]
+
+// = = = = = 
+
+// Insertion Sort Algorithm
+// Problem Statement: Given an array of N integers, write a program to implement the Insertion sorting algorithm.
+const insertionSort = (arr: number[]): void => {
+    for (let i = 1; i < arr.length; i++) {
+        let j = i;
+        while (j > 0 && arr[j - 1] > arr[j]) {
+            // Swap elements
+            let temp = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
+    }
+  console.log(arr);
+}
+
+// = = = = = 
+
