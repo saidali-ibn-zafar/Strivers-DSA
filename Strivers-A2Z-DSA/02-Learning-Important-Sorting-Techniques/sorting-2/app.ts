3637dd0b1a8548058c98e85f19918a3ab8fe2de4
saidali_ibn_zafar <120341849+saidali-ibn-zafar@users.noreply.github.com>
2024-02-12 23:43:18 +0100

    added recursiveInsertionSort

--- Strivers-A2Z-DSA/02-Learning-Important-Sorting-Techniques/sorting-2/app.ts
+++ Strivers-A2Z-DSA/02-Learning-Important-Sorting-Techniques/sorting-2/app.ts
@@ -22,3 +22,20 @@ recursiveBubbleSort(arr);
 console.log(arr); // [ 9, 13, 20, 24, 46, 52 ]
 
 // = = = = = =
+
+// Recursive Insertion Sort Algorithm
+// Problem Statement: Given an array of N integers, write a program to implement the Recursive Insertion Sort algorithm.
+const recursiveInsertionSort = (arr: number[], i: number = 0, n: number = arr.length): number[] => {
+    if (i === n) return arr;
+
+    let j = i;
+    while (j > 0 && arr[j - 1] > arr[j]) {
+        // Swap arr[j] and arr[j - 1]
+        [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
+        j--;
+    }
+
+    return recursiveInsertionSort(arr, i + 1, n);
+}
+
+console.log(recursiveInsertionSort([4, 2, 5, 3, 5, 6])); // [ 2, 3, 4, 5, 5, 6 ]
